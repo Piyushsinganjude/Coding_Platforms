155a26eeb030c8f3175bd90b5b63eafb5b5a8aa4
Piyush Singanjude <103660846+Piyushsinganjude@users.noreply.github.com>
2023-01-15 22:29:57 +0530

    Create Bubble Sort.cpp

---
+++ Geeks for geeks/Bubble Sort.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+    public:
+    //Function to sort the array using bubble sort algorithm.
+    void bubbleSort(int arr[], int n)
+    {
+        for(int i=0;i<n-1;i++){
+            for(int j =0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]= temp;
+                }
+            }
+        }
+    }
+};
+Link - https://practice.geeksforgeeks.org/problems/bubble-sort/1?page=1&difficulty[]=0&category[]=Sorting&sortBy=submissions
