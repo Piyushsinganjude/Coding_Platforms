8549a0241775596050055fc55b34799431f582df
Piyush Singanjude <103660846+Piyushsinganjude@users.noreply.github.com>
2023-01-22 22:38:22 +0530

    Create Array Partition.cpp

---
+++ LeetCode/Array Partition.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int ans = 0;
+        for(int i =0;i<nums.size();i++)
+        {
+            if(i%2==0)
+            ans +=nums[i];
+        }
+        return ans;
+    }
+};
+Link - (https://leetcode.com/problems/array-partition/description/)
