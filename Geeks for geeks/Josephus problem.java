01515cc31e657e8092622f9342b6367a87210f23
Piyush Singanjude <103660846+Piyushsinganjude@users.noreply.github.com>
2023-03-04 00:17:05 +0530

    Create Josephus problem.java

---
+++ Geeks for geeks/Josephus problem.java
@@ -0,0 +1,14 @@
+class Solution
+{
+   public int josephus(int n, int k)
+    {
+        return jos(n,k)+1;
+    }
+    int jos(int n , int k){
+        
+        if(n==1) return 0;
+        return (jos(n-1,k)+k)%n;
+    }
+
+}
+Link - (https://practice.geeksforgeeks.org/problems/josephus-problem/1?page=1&difficulty[]=0&category[]=Recursion&sortBy=submissions)
