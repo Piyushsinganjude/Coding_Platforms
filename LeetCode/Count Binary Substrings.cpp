eafd280fbdd285cabb9a39d5a60581965ed70c0e
Piyush Singanjude <103660846+Piyushsinganjude@users.noreply.github.com>
2023-03-20 23:49:05 +0530

    Create  Count Binary Substrings.cpp

---
+++ LeetCode/Count Binary Substrings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+     int prev =0;
+     int curr = 1;
+     int sum = 0;
+     for(int i =1;i<s.size();i++)
+     {
+         if(s[i]==s[i-1])
+         curr++;
+         else
+         {
+             sum +=min(prev,curr);
+             prev = curr;
+             curr = 1;
+
+         }
+     }
+       sum +=min(prev,curr);
+       return sum;
+    }
+};
+LINK = (https://leetcode.com/problems/count-binary-substrings/description/)
